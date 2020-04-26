b08d2dba607478483c6e0a35f723a1460e0173c4
EC2 Default User <ec2-user@ip-172-31-22-164.us-east-2.compute.internal>
2020-04-26 03:20:34 +0000

    last fix

--- index.js
+++ index.js
@@ -6,20 +6,33 @@ var app = express();
 /* Configure our server to read public folder and ejs files */
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
-/* Configure MySQL DBMS */
-const connection = mysql.createConnection({
-    // host: 'localhost',
-    // user: 'julio',
-    // password: 'julio',
-    // database: 'quotes_db'
+var db_config = {
     host:'us-cdbr-iron-east-01.cleardb.net',
     user:'b9b4fed8b49bc8',
     password:'e1092dd9',
     database:'heroku_d0d14298e1c6533'
-});
-connection.connect();
-
+};
+function handleDisconnect() {
+    console.log("In handleDisconnect");
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+    //   setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+handleDisconnect();
 /* The handler for the DEFAULT route */
 app.get('/', function(req, res){
     res.render('home');
